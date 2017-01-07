8ab7e9ea174f4be19366e98f0ffe7dac2dafae95
meissner <meissner@viada.de>
2017-01-07 17:34:34 +0100

    Added some group functionality in the list

--- webui/scripts/controllers/groups.js
+++ webui/scripts/controllers/groups.js
@@ -1,12 +1,26 @@
 'use strict';
 
 angular.module('webuiApp')
-    .controller('GroupsCtrl', function ($scope, watchApiService) {
+    .controller('GroupsCtrl', function ($scope, watchApiService, apiObjectHandler) {
 
-        $scope.groupsWs = null;
+        $scope.groups = [];
         $scope.init = function() {
             console.log("init GroupsCtrl");
 
+            // Load the groups
+            var scope = $scope;
+            apiObjectHandler.requestObject('groups', function(success, response) {
+                if (success) {
+                    scope.groups = response.data.items;
+                    console.log(scope.groups);
+                } else {
+                    // TODO: do additional error handling
+                    console.log('error');
+                }
+
+            });
+
+            // Subscribe for change
             $scope.groupsWs = watchApiService.watchApi('groups', 'GroupsCtrl', function(apiObject, message) {
                 // Callback if something changes
                 console.log('Got a change for ' + apiObject);
